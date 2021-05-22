e551726e2d107eed54f3c33de5b99fbb870eab6f
Eric Tarter <etarter00@gmail.com>
2021-05-22 09:41:28 -0700

    form try 5

--- src/components/NamePopup.js
+++ src/components/NamePopup.js
@@ -50,6 +50,23 @@ export const NamePopup = () => {
           value='Submit'
         />
       </form>
+      <form name='contact' method='POST' data-netlify='true'>
+        <p>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name' name='name' />
+        </p>
+        <p>
+          <label htmlFor='email'>Email</label>
+          <input type='text' id='email' name='email' />
+        </p>
+        <p>
+          <label htmlFor='message'>Message</label>
+          <textarea id='message' name='message'></textarea>
+        </p>
+        <p>
+          <button type='submit'>Send</button>
+        </p>
+      </form>
     </div>
   );
 };
